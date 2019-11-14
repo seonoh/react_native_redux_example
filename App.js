f5d9f5bbb9cbeba6084efab6adeebbe9e8606f56
seonoh <eso0609@naver.com>
2019-11-14 17:02:46 +0900

    apply store with Provider

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Count from './components/Count';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>React Native Redux Example</Text>
-      <Count/>
-      
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>React Native & Redux Example</Text>
+        <Count/>
+      </View>
+    </Provider>
   );
 };
 
